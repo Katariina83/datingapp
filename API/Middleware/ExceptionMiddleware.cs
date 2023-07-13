36922c4f140c49753c82dfa711b816b59d06c5f0
katariina <rikkonenkatariina@gmail.com>
2023-07-13 11:29:18 +0300

    Update AppUser class with new properties, add new entity framework migration for user characteristics
    Ch 86

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,6 +1,5 @@
 using System.Net;
 using System.Text.Json;
-using API.Errors;
 
 namespace API.Middleware
 {
@@ -28,15 +27,15 @@ namespace API.Middleware
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-                var response = _env.IsDevelopment()
-                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                    : new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+                //var response = _env.IsDevelopment()
+                    //? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    //: new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
 
-                    var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+                    //var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
-                    var json = JsonSerializer.Serialize(response, options);
+                    //var json = JsonSerializer.Serialize(response, options);
 
-                    await context.Response.WriteAsync(json);
+                    //await context.Response.WriteAsync(json);
             }
         }
     }
